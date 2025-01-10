9c846726febdcca41e805a4dd8b431e5110016a0
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-01-10 17:32:31 +0900

    WebGPUの初期化

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+
+async function main() {
+    const canvas = document.querySelector('canvas');
+    if (!canvas) {
+        throw new Error();
+    }
+
+    // WebGPUコンテキストの取得
+    const context = canvas.getContext('webgpu') as GPUCanvasContext | null;
+    if (!context) {
+        throw new Error();
+    }
+
+    // deviceの取得
+    const g_adapter = await navigator.gpu.requestAdapter();
+    if (!g_adapter) {
+        throw new Error();
+    }
+
+    const g_device = await g_adapter.requestDevice();
+
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+    context.configure({
+        device: g_device,
+        format: presentationFormat,
+        alphaMode: 'opaque',
+    });
+}
+
+main();
