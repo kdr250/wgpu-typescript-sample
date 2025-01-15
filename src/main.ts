0668a7bf606c65cd41b5eb2bf4d772bd784fd1d2
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-01-15 15:23:17 +0900

    Revert to WebGPUで三角形を描画する

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import vertexShader from './shader/vertex.wgsl?raw'
 import fragmentShader from './shader/fragment.wgsl?raw'
 
-function frame(device: GPUDevice, pipeline: GPURenderPipeline, texture: GPUTexture) {
+function frame(device: GPUDevice, context: GPUCanvasContext, pipeline: GPURenderPipeline) {
     const commandEncoder = device.createCommandEncoder();
-
+    const textureView = context.getCurrentTexture().createView();
     const renderPassDescriptor: GPURenderPassDescriptor = {
         colorAttachments: [
             {
-                view: texture.createView(),
+                view: textureView,
                 clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
                 loadOp: 'clear',
                 storeOp: 'store',
             },
         ],
     };
-
     const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
     passEncoder.setPipeline(pipeline);
     passEncoder.draw(3, 1, 0, 0);
@@ -23,61 +22,17 @@ function frame(device: GPUDevice, pipeline: GPURenderPipeline, texture: GPUTextu
     device.queue.submit([commandEncoder.finish()]);
 }
 
-function drawImageFromBuffer(arrayBuffer: ArrayBuffer, width: number, height: number) {
-    // キャンバスを作成
-    const canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext('2d');
-    if (!context) {
+async function main() {
+    const canvas = document.querySelector('canvas');
+    if (!canvas) {
         throw new Error();
     }
 
-    // ImageDataオブジェクトを作成
-    const imageData = new ImageData(
-        new Uint8ClampedArray(arrayBuffer),
-        width,
-        height,
-    );
-
-    // キャンバスにImageDataを描画
-    context.putImageData(imageData, 0, 0);
-
-    return canvas;
-}
-
-async function getTextureDataAndShowAsCanvas(device: GPUDevice, texture: GPUTexture) {
-    // GPUBufferを作成（テクスチャのデータを読み出すため）
-    const bufferSize = 256 * 256 * 4;
-    const buffer = device.createBuffer({
-        size: bufferSize,
-        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
-    });
-
-    const commandEncoder = device.createCommandEncoder();
-
-    commandEncoder.copyTextureToBuffer(
-        { texture: texture },
-        { buffer: buffer, bytesPerRow: 256 * 4 },
-        { width: 256, height: 256, depthOrArrayLayers: 1 },
-    );
-
-    // コマンドをキューに送信して実行
-    device.queue.submit([commandEncoder.finish()]);
-
-    // データを読み出す
-    await buffer.mapAsync(GPUMapMode.READ);
-    const arrayBuffer = buffer.getMappedRange();
-
-    // 画像を表示
-    const canvas = drawImageFromBuffer(arrayBuffer, 256, 256);
-
-    document.body.appendChild(canvas);
-
-    buffer.unmap();
-}
-
-async function main() {
+    // WebGPUコンテキストの取得
+    const context = canvas.getContext('webgpu') as GPUCanvasContext | null;
+    if (!context) {
+        throw new Error();
+    }
 
     // deviceの取得
     const g_adapter = await navigator.gpu.requestAdapter();
@@ -87,7 +42,15 @@ async function main() {
 
     const device = await g_adapter.requestDevice();
 
-    // 三角形を描画するときのRenderPipeline
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+    context.configure({
+        device: device,
+        format: presentationFormat,
+        alphaMode: 'opaque',
+    });
+
+    // create a render pipeline
     const pipeline = device.createRenderPipeline({
         layout: 'auto',
         vertex: {
@@ -103,7 +66,7 @@ async function main() {
             entryPoint: 'main',
             targets: [
                 {
-                    format: 'rgba8unorm',
+                    format: presentationFormat, // @location(0) in fragment shader
                 }
             ]
         },
@@ -112,16 +75,7 @@ async function main() {
         },
     });
 
-    // Create texture
-    const texture = device.createTexture({
-        size: [256, 256],
-        format: 'rgba8unorm',
-        usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
-    });
-
-    frame(device, pipeline, texture);
-
-    await getTextureDataAndShowAsCanvas(device, texture);
+    frame(device, context, pipeline);
 }
 
 main();
