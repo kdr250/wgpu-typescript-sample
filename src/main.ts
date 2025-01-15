73b5e690aa11df29bbb6526f065854d145fa00aa
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-01-15 15:06:27 +0900

    GPUTextureからGPUBufferを経由してArrayBufferを取り出す

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,60 @@ function frame(device: GPUDevice, pipeline: GPURenderPipeline, texture: GPUTextu
     device.queue.submit([commandEncoder.finish()]);
 }
 
+function drawImageFromBuffer(arrayBuffer: ArrayBuffer, width: number, height: number) {
+    // キャンバスを作成
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext('2d');
+    if (!context) {
+        throw new Error();
+    }
+
+    // ImageDataオブジェクトを作成
+    const imageData = new ImageData(
+        new Uint8ClampedArray(arrayBuffer),
+        width,
+        height,
+    );
+
+    // キャンバスにImageDataを描画
+    context.putImageData(imageData, 0, 0);
+
+    return canvas;
+}
+
+async function getTextureDataAndShowAsCanvas(device: GPUDevice, texture: GPUTexture) {
+    // GPUBufferを作成（テクスチャのデータを読み出すため）
+    const bufferSize = 256 * 256 * 4;
+    const buffer = device.createBuffer({
+        size: bufferSize,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
+    });
+
+    const commandEncoder = device.createCommandEncoder();
+
+    commandEncoder.copyTextureToBuffer(
+        { texture: texture },
+        { buffer: buffer, bytesPerRow: 256 * 4 },
+        { width: 256, height: 256, depthOrArrayLayers: 1 },
+    );
+
+    // コマンドをキューに送信して実行
+    device.queue.submit([commandEncoder.finish()]);
+
+    // データを読み出す
+    await buffer.mapAsync(GPUMapMode.READ);
+    const arrayBuffer = buffer.getMappedRange();
+
+    // 画像を表示
+    const canvas = drawImageFromBuffer(arrayBuffer, 256, 256);
+
+    document.body.appendChild(canvas);
+
+    buffer.unmap();
+}
+
 async function main() {
 
     // deviceの取得
@@ -66,6 +120,8 @@ async function main() {
     });
 
     frame(device, pipeline, texture);
+
+    await getTextureDataAndShowAsCanvas(device, texture);
 }
 
 main();
