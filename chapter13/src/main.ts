eb81617bc2616198b471f7a0c633252eef0340ec
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-01-16 11:48:07 +0900

    chapter13を復活

---
+++ chapter13/src/main.ts
@@ -0,0 +1,95 @@
+import vertexShader from './shader/vertex.wgsl?raw'
+import fragmentShader from './shader/fragment.wgsl?raw'
+
+function buildRenderBundle(device: GPUDevice, pipeline: GPURenderPipeline): GPURenderBundle {
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+    const renderBundleDescriptor: GPURenderBundleEncoderDescriptor = {
+        colorFormats: [presentationFormat],
+    };
+
+    const encoder = device.createRenderBundleEncoder(renderBundleDescriptor);
+    encoder.setPipeline(pipeline);
+    encoder.draw(3, 1, 0, 0);
+    const renderBundle = encoder.finish();
+    return renderBundle;
+}
+
+function frame(device: GPUDevice, context: GPUCanvasContext, renderBundle: GPURenderBundle) {
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = context.getCurrentTexture().createView();
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+        colorAttachments: [
+            {
+                view: textureView,
+                clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+                loadOp: 'clear',
+                storeOp: 'store',
+            },
+        ],
+    };
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.executeBundles([renderBundle]);
+    passEncoder.end();
+
+    device.queue.submit([commandEncoder.finish()]);
+}
+
+async function main() {
+    const canvas = document.querySelector('canvas');
+    if (!canvas) {
+        throw new Error();
+    }
+
+    // WebGPUコンテキストの取得
+    const context = canvas.getContext('webgpu') as GPUCanvasContext | null;
+    if (!context) {
+        throw new Error();
+    }
+
+    // deviceの取得
+    const g_adapter = await navigator.gpu.requestAdapter();
+    if (!g_adapter) {
+        throw new Error();
+    }
+
+    const device = await g_adapter.requestDevice();
+
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+    context.configure({
+        device: device,
+        format: presentationFormat,
+        alphaMode: 'opaque',
+    });
+
+    // create a render pipeline
+    const pipeline = device.createRenderPipeline({
+        layout: 'auto',
+        vertex: {
+            module: device.createShaderModule({
+                code: vertexShader,
+            }),
+            entryPoint: 'main',
+        },
+        fragment: {
+            module: device.createShaderModule({
+                code: fragmentShader,
+            }),
+            entryPoint: 'main',
+            targets: [
+                {
+                    format: presentationFormat, // @location(0) in fragment shader
+                }
+            ]
+        },
+        primitive: {
+            topology: 'triangle-list'
+        },
+    });
+
+    const renderBundle = buildRenderBundle(device, pipeline);
+
+    frame(device, context, renderBundle);
+}
+
+main();
